6aae739274cbe80b7af34bc9be2900161482043f
n4to4 <n4to4k@gmail.com>
2021-07-28 20:35:38 +0900

    snapshot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::{thread::sleep, time::Duration};
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let one = tokio::spawn(greet());
+    let two = tokio::spawn(greet());
+    let (_, _) = tokio::join!(one, two);
+}
+
+async fn greet() {
     println!("Hello!");
     sleep(Duration::from_millis(500));
     println!("Goodbye!");
