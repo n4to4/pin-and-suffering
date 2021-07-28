62537531992536743e4cefcad91d1ceb9e7cf142
n4to4 <n4to4k@gmail.com>
2021-07-28 20:53:41 +0900

    poll many

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ struct MyFuture {}
 impl Future for MyFuture {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
-        Poll::Ready(())
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        println!("MyFuture::poll");
+        cx.waker().wake_by_ref();
+        Poll::Pending
     }
 }
