6ae88f88a7d32656dcf38934e39e2d9d4b251b7e
n4to4 <n4to4k@gmail.com>
2021-07-28 21:00:57 +0900

    poll once; done

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,36 @@ use std::{
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let _fut = MyFuture {}.await;
+    let _fut = MyFuture::new().await;
 }
 
-struct MyFuture {}
+struct MyFuture {
+    slept: bool,
+}
+
+impl MyFuture {
+    fn new() -> Self {
+        Self { slept: false }
+    }
+}
 
 impl Future for MyFuture {
     type Output = ();
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         println!("MyFuture::poll");
 
-        let waker = cx.waker().clone();
-        std::thread::spawn(move || {
-            std::thread::sleep(Duration::from_secs(1));
-            waker.wake();
-        });
-
-        Poll::Pending
+        match self.slept {
+            false => {
+                let waker = cx.waker().clone();
+                std::thread::spawn(move || {
+                    std::thread::sleep(Duration::from_secs(1));
+                    waker.wake();
+                });
+                self.slept = true;
+                Poll::Pending
+            }
+            true => Poll::Ready(()),
+        }
     }
 }
