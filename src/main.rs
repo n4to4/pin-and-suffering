ca1cd8233c56078c610a7bf2b13fad4663a53f84
n4to4 <n4to4k@gmail.com>
2021-07-29 17:34:23 +0900

    slow read

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,22 @@
-use futures::FutureExt;
 use std::{
-    future::Future,
     pin::Pin,
     task::{Context, Poll},
-    time::Duration,
 };
 use tokio::{
     fs::File,
     io::{AsyncRead, AsyncReadExt, ReadBuf},
     time::Instant,
-    time::Sleep,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), tokio::io::Error> {
     let mut buf = vec![0u8; 128 * 1024];
-    let mut f = File::open("/dev/urandom").await?;
+    let mut f = SlowRead::new(File::open("/dev/urandom").await?);
     let before = Instant::now();
     f.read_exact(&mut buf).await?;
     println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
 
     Ok(())
-}
-
-struct MyFuture {
-    sleep: Pin<Box<Sleep>>,
-}
-
-impl MyFuture {
-    fn new() -> Self {
-        Self {
-            sleep: Box::pin(tokio::time::sleep(Duration::from_secs(1))),
-        }
-    }
-}
-
-impl Future for MyFuture {
-    type Output = ();
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        println!("MyFuture::poll");
-        self.sleep.poll_unpin(cx)
-    }
 }
 
 struct SlowRead<R> {
