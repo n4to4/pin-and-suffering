510e9917193f5c0bdbf2a03b23680f0977219d82
n4to4 <n4to4k@gmail.com>
2021-07-29 17:32:57 +0900

    read

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,22 @@ use std::{
     task::{Context, Poll},
     time::Duration,
 };
-use tokio::time::Sleep;
+use tokio::{
+    fs::File,
+    io::{AsyncRead, AsyncReadExt, ReadBuf},
+    time::Instant,
+    time::Sleep,
+};
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let _fut = MyFuture::new().await;
+#[tokio::main]
+async fn main() -> Result<(), tokio::io::Error> {
+    let mut buf = vec![0u8; 128 * 1024];
+    let mut f = File::open("/dev/urandom").await?;
+    let before = Instant::now();
+    f.read_exact(&mut buf).await?;
+    println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
+
+    Ok(())
 }
 
 struct MyFuture {
@@ -30,5 +41,30 @@ impl Future for MyFuture {
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         println!("MyFuture::poll");
         self.sleep.poll_unpin(cx)
+    }
+}
+
+struct SlowRead<R> {
+    reader: Pin<Box<R>>,
+}
+
+impl<R> SlowRead<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader: Box::pin(reader),
+        }
+    }
+}
+
+impl<R> AsyncRead for SlowRead<R>
+where
+    R: AsyncRead,
+{
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        self.reader.as_mut().poll_read(cx, buf)
     }
 }
