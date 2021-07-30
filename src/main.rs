64636b36f603361b1297624eb74876e4b89de1e9
n4to4 <n4to4k@gmail.com>
2021-07-30 20:26:40 +0900

    pass

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,10 @@ async fn main() -> Result<(), tokio::io::Error> {
     let mut buf = vec![0u8; 128 * 1024];
     let mut f = SlowRead::new(File::open("/dev/urandom").await?);
     let before = Instant::now();
+
     let mut f = unsafe { Pin::new_unchecked(&mut f) };
     f.read_exact(&mut buf).await?;
+
     println!("Read {} bytes in {:?}", buf.len(), before.elapsed());
 
     Ok(())
@@ -41,16 +43,22 @@ where
     R: AsyncRead + Unpin,
 {
     fn poll_read(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        let mut sleep = unsafe { self.as_mut().map_unchecked_mut(|this| &mut this.sleep) };
+        // pin-project both fields
+        let (mut sleep, reader) = unsafe {
+            let this = self.get_unchecked_mut();
+            (
+                Pin::new_unchecked(&mut this.sleep),
+                Pin::new_unchecked(&mut this.reader),
+            )
+        };
+
         match sleep.poll_unpin(cx) {
             Poll::Ready(_) => {
-                let sleep = unsafe { self.as_mut().map_unchecked_mut(|this| &mut this.sleep) };
                 sleep.reset(Instant::now() + Duration::from_millis(25));
-                let reader = unsafe { self.as_mut().map_unchecked_mut(|this| &mut this.reader) };
                 reader.poll_read(cx, buf)
             }
             Poll::Pending => Poll::Pending,
